213aeede7c45bb838f1285f12a0a8d59216ba05d
yinghaoawang <yinghao.a.wang@gmail.com>
2019-04-24 17:01:16 -0700

    further polish to text box inputs

--- src/003-dope-wars/input/text/NumberField.js
+++ src/003-dope-wars/input/text/NumberField.js
@@ -41,17 +41,23 @@ export default class NumberTextField extends Text {
         });
 
         this.scene.input.on('pointerdown', (pointer) => {
-            
             if (this.scene.selectedInput === this) {
                 let absX = this.parentContainer.x + this.x;
                 let absY = this.parentContainer.y + this.y;
                 if (pointer.x >= absX && pointer.x <= absX + this.trueWidth && pointer.y >= absY && pointer.y <= absY + this.trueHeight) {
                     // do nothing
+                    
                 } else {
                     // if pointer up 
                     this.scene.selectedInput = null;
+                    if (this.trueText.length == 0) {
+                        this.trueText = '0';
+                    }
                     this.setText(this.trueText + '');
                 }
+            } else if (this.trueText.length == 0) {
+                this.trueText = '0';
+                this.setText(this.trueText + '');
             }
         });
 
@@ -63,9 +69,16 @@ export default class NumberTextField extends Text {
                     if (this.trueText.length < this.maxTrueText) {
                         this.trueText += key;
                     }
+                } else if (key == 'Backspace') {
+                    if (this.trueText.length > 0) {
+                        this.trueText = this.trueText.substring(0, this.trueText.length - 1);
+                    }
+                } else if (key == 'Enter') {
+                    this.scene.selectedInput = null;
+                    if (this.trueText.length == 0) this.trueText = '0';
                 }
-
-                if (this.trueText.length < this.maxTrueText) {
+                    
+                if (this.scene.selectedInput != null && (this.trueText.length < this.maxTrueText)) {
                     this.setText(this.trueText + '_');
                 } else {
                     this.setText(this.trueText + '');
